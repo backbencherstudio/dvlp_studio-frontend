31edad18433af7f7343ac325a3b9ca58c305adf1
hrid1 <hridoycse1@gmail.com>
2025-11-19 09:00:03 +0600

    Fix suspense Error

---
+++ app/(auth)/auth/forgot-password/_components/ForgotPassword.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { privateAxios } from "@/lib/axios";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import Image from "next/image";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type") || "student";
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await privateAxios.post("/auth/forgot-password", { email });
+      toast.success("OTP sent to your email");
+
+      // Redirect to reset-password page with email in query
+      router.push(
+        `/auth/reset-password?type=${type}&email=${encodeURIComponent(email)}`
+      );
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+    }
+  };
+
+  return (
+    <div className="border [background:rgba(255,255,255,0.10)] backdrop-blur-[5px] rounded-3xl border-solid border-[rgba(255,255,255,0.20)] mt-8 p-8 max-w-md mx-auto">
+      <Image
+        className="w-[clamp(150px,12vw,225.055px)] h-auto mx-aut"
+        src="/evolve-logo.png"
+        alt=""
+        width={225}
+        height={100}
+      />
+      <div className="max-w-md mx-auto mt-6 p-6 border border-gray-50 rounded-lg">
+        <h2 className="text-xl font-semibold mb-4 text-white">
+          Forgot Password
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            className="w-full border px-3 py-2 rounded text-white"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <button
+            type="submit"
+            className="justify-center items-center gap-[7.637px] [background:linear-gradient(90deg,#A855F7_0%,#EC4899_100%)]  px-0 py-4 rounded-xl text-white w-full cursor-pointer hover:[background:linear-gradient(90deg,#A855F7_0%,#6366F1_100%)] transition-colors duration-300 text-base font-bold leading-6 disabled:opacity-60 disabled:cursor-not-allowed"
+          >
+            Send OTP
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/(auth)/auth/reset-password/page.tsx
+++ app/(auth)/auth/reset-password/page.tsx
@@ -1,82 +1,10 @@
-"use client";
+import React, { Suspense } from "react";
+import ResetPasswordPage from "./_components/ResetPassword";
 
-import { useState } from "react";
-import { privateAxios } from "@/lib/axios";
-import { useSearchParams, useRouter } from "next/navigation";
-import { toast } from "sonner";
-
-export default function ResetPasswordPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  const role = searchParams.get("type");
-  const emailFromQuery = searchParams.get("email") || "";
-
-  console.log(role)
-
-  const [token, setToken] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    if (!emailFromQuery) {
-      toast.error("Email is missing. Please start from Forgot Password page.");
-      return;
-    }
-
-    try {
-      await privateAxios.post("/auth/reset-password", {
-        email: emailFromQuery,
-        token,
-        password,
-      });
-
-      toast.success("Password reset successful!");
-      router.push(`/${role}/sign-in`); // redirect to login page
-    } catch (error: any) {
-      toast.error(error?.response?.data?.message || "Failed to reset password");
-    }
-  };
-
+export default function page() {
   return (
-    <div  className="border [background:rgba(255,255,255,0.10)] backdrop-blur-[5px] rounded-3xl border-solid border-[rgba(255,255,255,0.20)] mt-8 p-8 w-full max-w-md">
-      <h2 className="text-xl font-semibold mb-4 text-white">Reset Password</h2>
-
-      <form onSubmit={handleSubmit} className="space-y-4">
-        {/* Show email readonly */}
-        <input
-          type="email"
-          value={emailFromQuery}
-          readOnly
-          className="w-full border px-3 py-2 rounded bg-gray-100 focus:outline-0"
-        />
-
-        <input
-          type="text"
-          placeholder="OTP Code"
-          className="w-full border px-3 py-2 rounded bg-white"
-          value={token}
-          onChange={(e) => setToken(e.target.value)}
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="New Password"
-          className="w-full border px-3 py-2 rounded bg-white"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-
-        <button
-          type="submit"
-          className="flex justify-center items-center gap-[7.637px] [background:linear-gradient(90deg,#A855F7_0%,#EC4899_100%)]  px-0 py-4 rounded-xl text-white w-full cursor-pointer hover:[background:linear-gradient(90deg,#A855F7_0%,#6366F1_100%)] transition-colors duration-300 text-base font-bold leading-6 disabled:opacity-60 disabled:cursor-not-allowed"
-        >
-          Reset Password
-        </button>
-      </form>
-    </div>
+    <Suspense fallback={"Loading..."}>
+      <ResetPasswordPage/>
+    </Suspense>
   );
 }
